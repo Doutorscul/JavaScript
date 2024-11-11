0d3d6d620513190bf48c10da4e92330a96300f30
Doutorscul <douglas.figueiredo@ufu.br>
2024-11-11 18:30:47 -0300

    Initial commit

---
+++ aula10/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+  }
+console.log ('Dirija usando cinto de segurança!')
+
